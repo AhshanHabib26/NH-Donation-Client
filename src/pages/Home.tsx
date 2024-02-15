9f4b50e74acf4205f21b36ed01cdd0e63a5b7bee
AhshanHabib26 <habib.net27@gmail.com>
2024-02-15 17:01:24 +0600

    Gallary Added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import Banner from "../components/ui/Banner";
 import BannerDescription from "../components/ui/BannerDescription";
+import Gallary from "../components/ui/Gallary";
 import WinterClothes from "../components/ui/WinterClothes";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <Banner />
       <BannerDescription />
       <WinterClothes />
+      <Gallary />
     </div>
   );
 }
